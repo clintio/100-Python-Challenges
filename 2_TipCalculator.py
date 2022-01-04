627d7da3b9181f54a14c9530769eeff2bc99e429
Clinton <clinton.humphrey21@gmail.com>
2022-01-04 00:44:28 -0800

    Update and rename 2_TipCalculator.md to 2_TipCalculator.py

--- 2_TipCalculator.py
+++ 2_TipCalculator.py
@@ -1,11 +1,11 @@
-## Source  
-https://replit.com/@clintio/tip-calculator-start#main.py  
+# Source- https://www.udemy.com/course/100-days-of-code/learn/lecture/18029131#overview  
+# My code- https://replit.com/@clintio/tip-calculator-start#main.py  
 
 ## Python script  
-#Calculate how much everyone owes if the bill is
-* $150.00 total
-* Split between 5 people 
-* 12% tip 
+# Calculate how much everyone owes if the bill is
+# $150.00 total
+# Split between 5 people 
+# 12% tip 
 
 #Each person should pay ($150.00 / 5 people) * 1.12 tip = $33.6  
 #Format the result to 2 decimal places = $33.60
